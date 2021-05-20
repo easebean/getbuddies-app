e0159d9c28ec4403a6e542a7fbfcac2b03d8520b
sachin4653 <sachinjaisawal0@gmail.com>
2021-05-20 12:16:17 +0530

    Updated the update user form

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+public display:boolean=true;
 
+name:string;
+userName:string;
+phoneNumber:string;
+country:string;
+email:string;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeMode() : boolean{
+ 
+   return this.display=!this.display;
   }
 
 }
