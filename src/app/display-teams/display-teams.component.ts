4fa1f02e5e779f10ee73b6fc098eeec7e6df51b6
iankityadav <1002ankit@gmail.com>
2021-05-25 20:11:09 +0530

    updated functions

--- src/app/display-teams/display-teams.component.ts
+++ src/app/display-teams/display-teams.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { Room } from '../room';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-display-teams',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-teams.component.css']
 })
 export class DisplayTeamsComponent implements OnInit {
-
-  constructor() { }
+  rooms:Room[]
+  constructor(private userService:UserService,private app:AppComponent) { }
 
   ngOnInit(): void {
+    if(this.app.loggedUser.id!==undefined)
+    this.getRooms()
   }
-
+  getRooms(){
+    this.userService.rooms(this.app.loggedUser.id).subscribe(
+      (response:any) => {
+        this.rooms = response
+        console.log(response);
+        
+      },
+      (error:HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    )
+  }
 }
