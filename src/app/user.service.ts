55aac73a81ace5858621a01998d095850c0d5d1f
iankityadav <1002ankit@gmail.com>
2021-05-25 20:08:23 +0530

    added services

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { Room } from "./room";
 import { User } from "./user";
 
 @Injectable({
@@ -15,7 +16,16 @@ export class UserService{
     public create(user:User):Observable<User>{
         return this.http.post<User>(`${this.apiUrl}/user/add`,user)
     }
+    
     public login(userName:string,password:string):Observable<User>{
         return this.http.get<User>(`${this.apiUrl}/user/login/${userName}/${password}`)
+    }
+
+    public update(user:User):Observable<User>{
+        return this.http.put<User>(`${this.apiUrl}/user/update`,user)
+    }
+
+    public rooms(userId:number):Observable<Room>{
+        return this.http.get<Room>(`${this.apiUrl}/user/room/${userId}/`)
     }
 }
