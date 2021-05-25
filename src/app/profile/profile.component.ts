1cdcce106ec2368f1114cf2af2c906205553a2e8
iankityadav <1002ankit@gmail.com>
2021-05-25 22:02:31 +0530

    fix: routes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AppComponent } from '../app.component';
 import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-profile',
@@ -11,22 +12,28 @@ import { User } from '../user';
 export class ProfileComponent implements OnInit {
 
   display:boolean=true;
+  isLogged:boolean = false
   name: string;
   userName: string;
   email: string;
   phoneNumber: string;
   city: string;
   user: User
-  constructor(private app:AppComponent,private router:Router) { }
+  constructor(private app:AppComponent,private router:Router,private route: ActivatedRoute,private userService:UserService) { }
 
   ngOnInit(): void {
-    this.user = this.app.loggedUser
-    if(this.user===undefined)
-    this.router.navigate(['/login'])
+    const routeParams = this.route.snapshot.paramMap;
+    const userIdFromRoute = Number(routeParams.get('userId'));
+    this.userService.find(userIdFromRoute).subscribe(
+      (response:any) => {
+        this.user = response
+        console.log(this.user);
+      }
+    )
+    this.isLogged = this.app.isAuth()
   }
 
   changeMode() : boolean{
- 
    return this.display=!this.display;
   }
 
